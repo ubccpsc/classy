3294001f48d0f3647d6c9517de9f21311e39788c
Nick Bradley <nickbradley@live.ca>
2018-02-10 15:25:56 -0800

    Combine stdout and stderr

--- src/docker/DockerUtil.ts
+++ src/docker/DockerUtil.ts
@@ -4,24 +4,23 @@ import {IDockerCmdResult} from "./DockerTypes";
 export default class DockerUtil {
     public static async execCmd(args: string[], options: SpawnOptions = {}): Promise<IDockerCmdResult> {
         return new Promise<IDockerCmdResult>((resolve, reject) => {
-            let stdout: Buffer = Buffer.alloc(0);
-            let stderr: Buffer = Buffer.alloc(0);
+            let output: Buffer = Buffer.allocUnsafe(0);
 
             const cmd: ChildProcess = spawn(`docker`, args, options);
             cmd.on(`error`, (err) => {
                 reject(err);
              });
             cmd.stdout.on(`data`, (data: Buffer) => {
-                stdout = Buffer.concat([stdout, data], stdout.length + data.length);
+                output = Buffer.concat([output, data], output.length + data.length);
             });
             cmd.stderr.on(`data`, (data: Buffer) => {
-                stderr = Buffer.concat([stderr, data], stderr.length + data.length);
+                output = Buffer.concat([output, data], output.length + data.length);
             });
             cmd.on(`close`, (code, signal) => {
                 if (code === 0) {
-                    resolve({code, output: stdout.toString().trim()});
+                    resolve({code, output: output.toString().trim()});
                 } else {
-                    reject({code, output: stderr.toString().trim()});
+                    reject({code, output: output.toString().trim()});
                 }
             });
         });
