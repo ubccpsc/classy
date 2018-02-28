f41b32e8b538ad56604b5e2bdc41ebbd37cd872c
Nick Bradley <nickbradley@live.ca>
2018-02-28 10:55:20 -0800

    save container log

--- grader/src/server/RouteHandler.ts
+++ grader/src/server/RouteHandler.ts
@@ -150,6 +150,7 @@ export default class RouteHandler {
             if (typeof body.container.logSize !== "undefined") {
                 log = log.substring(0, body.container.logSize);
             }
+            fs.writeFile(log, `${keepDir}/stdio.txt`);
             Log.info("Route Done");
 
             // Generate response
