18fa4dda88e7345db8ae7dfc210df6444c489299
Nick Bradley <nickbradley@live.ca>
2018-05-02 09:37:38 -0500

    organize project using yarn workspaces

--- common/Config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* Wrapper for process.env */
-Object.defineProperty(exports, "__esModule", { value: true });
-var Config = /** @class */ (function () {
-    function Config() {
-    }
-    return Config;
-}());
-exports.Config = Config;
---
+++ packages/common/Config.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Config {
+}
+exports.Config = Config;
+//# sourceMappingURL=Config.js.map
