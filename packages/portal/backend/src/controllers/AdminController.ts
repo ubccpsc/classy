7282efa78c932fbe28a8ae8e911cc28d11b877a6
Reid Holmes (MBA) <rtholmes@gmail.com>
2018-12-10 13:28:06 -0800

    see #29 roll out of temporary code (worked, but not needed anymore)

--- packages/portal/backend/src/controllers/AdminController.ts
+++ packages/portal/backend/src/controllers/AdminController.ts
@@ -143,14 +143,6 @@ export class AdminController {
     public async getStudents(): Promise<StudentTransport[]> {
         const people = await this.pc.getAllPeople();
 
-        for (const person of people) {
-            if (person.kind === null) {
-                Log.info("fixing null person kind for: " + person.id);
-                person.kind = PersonKind.STUDENT;
-                await this.pc.writePerson(person);
-            }
-        }
-
         const students: StudentTransport[] = [];
         for (const person of people) {
             if (person.kind === PersonKind.STUDENT || person.kind === null) { // null should be set on first login
