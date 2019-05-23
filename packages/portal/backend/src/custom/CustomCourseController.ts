27c9edc4d1abaa742bebc72f5d22c9ef5d92e6d9
Reid Holmes (MBA) <rtholmes@gmail.com>
2019-05-22 21:00:58 -0700

    310 changes

--- packages/portal/backend/src/custom/CustomCourseController.ts
+++ packages/portal/backend/src/custom/CustomCourseController.ts
@@ -1,14 +1,14 @@
 import Log from "../../../../common/Log";
-import {CourseController} from "./CourseController";
-import {IGitHubController} from "./GitHubController";
+import {CourseController} from "../controllers/CourseController";
+import {IGitHubController} from "../controllers/GitHubController";
 
-export class CS310Controller extends CourseController {
+export class CustomCourseController extends CourseController {
 
     // Does not do anything right now, just here so we know where to make course-specific changes to CourseController
 
     public constructor(ghController: IGitHubController) {
         super(ghController);
-        Log.info("CS310Controller::<init>");
+        Log.info("CustomCourseController::<init>");
     }
 
 }
