99f0fa298ac9ff0185cd28b5b5b879560feaa99f
Anthony Winstanley <winstan@cs.ubc.ca>
2019-08-09 09:42:54 -0700

    Simplifications, also make script die if html dir doesn't exist.

--- helper-scripts/default-file-setup.sh
+++ helper-scripts/default-file-setup.sh
@@ -6,93 +6,65 @@
 #
 # steca - 
 # IMPORTANT: Aborts if any previous custom file is found.
+#
+# This should be run from the base folder, i.e. /opt/classy
 
-set e
+set -e
+
 version=1.0.0
+echo "default-file-setup.sh:: starting script version ${version}..."
+
 envFile=".env"
-envVar=''
+envVar=`awk -F = '/^NAME[[:space:]]*=/{gsub(/[[:space:]]/, "", $2); print $2}' ${envFile}`
+if [ $envVar == '' ]; then
+	echo "default-file-setup.sh:: ERROR - COULD NOT FIND NAME PROPERTY IN ${envFile}"
+	exit 1
+fi
+echo "default-file-setup.sh:: envVar = ${envVar}"
 
-setFilenames() {
-	backendFiles=(packages/portal/backend/src/custom/CustomCourseRoutes.ts
-		packages/portal/backend/src/custom/CustomCourseController.ts)
 
-	frontendFiles=(packages/portal/frontend/src/app/custom/CustomStudentView.ts
-		packages/portal/frontend/src/app/custom/CustomAdminView.ts
-		packages/portal/frontend/html/${envVar}/custom.html
-		packages/portal/frontend/html/${envVar}/landing.html
-		packages/portal/frontend/html/${envVar}/login.html
-		packages/portal/frontend/html/${envVar}/student.html
-	)
-}
+customFiles=(packages/portal/backend/src/custom/CustomCourseRoutes.ts
+	packages/portal/backend/src/custom/CustomCourseController.ts
+	packages/portal/frontend/src/app/custom/CustomStudentView.ts
+	packages/portal/frontend/src/app/custom/CustomAdminView.ts
+	packages/portal/frontend/html/${envVar}/custom.html
+	packages/portal/frontend/html/${envVar}/landing.html
+	packages/portal/frontend/html/${envVar}/login.html
+	packages/portal/frontend/html/${envVar}/student.html
+)
 
-function main() {
-	echo "default-file-setup.sh:: starting script version ${version}..."
-	cd ..
-	setEnvName
-	setFilenames
-	preCopy
-	copyFiles
-	exit 0
-}
 
-function preCopy() {
-	## 1. Check that files do not already exist.
-	doesFileExist "${frontendFiles[@]}"
-	doesFileExist "${backendFiles[@]}"
-}
-
+## 1. Check that files do not already exist.
 ## Exits if a matching file is found
-function doesFileExist() {
-	echo "default-file-setup.sh:: doesFileExist started"
-	filenameList=("$@")
-	for filename in "${filenameList[@]}"
-		do
-			if [ -f "${filename}" ]; then
-				echo "default-file-setup.sh:: doesFileExist ERROR: File ${filename} exists!"
-				echo "default-file-setup.sh:: CANNOT CREATE CUSTOM FILES IF A CUSTOM FILE ALREADY EXISTS. ALL CUSTOM FILES MUST BE REMOVED BEFORE RE-CREATING CUSTOM FILES FROM DEFAULTS"
-				exit 1
-			fi
-		done
-}
-
-## Creates Custom boilerplate files from Default files
-## -n flag to not overwrite just to be safe
-function copyFiles() {
-	echo "default-file-setup.sh:: copyFiles() started - BACKEND"
-	cp -nv "packages/portal/backend/src/custom/DefaultCourseRoutes.ts" "packages/portal/backend/src/custom/CustomCourseRoutes.ts"
-	cp -nv "packages/portal/backend/src/custom/DefaultCourseController.ts" "packages/portal/backend/src/custom/CustomCourseController.ts"
-
-	echo "default-file-setup.sh:: copyFiles() started - FRONTEND"
-	## VIEW MODELS
-	cp -nv "packages/portal/frontend/src/app/custom/DefaultStudentView.ts" "packages/portal/frontend/src/app/custom/CustomStudentView.ts"
-	cp -nv "packages/portal/frontend/src/app/custom/DefaultAdminView.ts" "packages/portal/frontend/src/app/custom/CustomAdminView.ts"
-
-	## HTML VIEWS
-	mkdir -p "packages/portal/frontend/html/${envVar}/"
-	cp -nv "packages/portal/frontend/html/default/custom.html" "packages/portal/frontend/html/${envVar}/custom.html"
-	cp -nv "packages/portal/frontend/html/default/landing.html" "packages/portal/frontend/html/${envVar}/landing.html"
-	cp -nv "packages/portal/frontend/html/default/login.html" "packages/portal/frontend/html/${envVar}/login.html"
-	cp -nv "packages/portal/frontend/html/default/student.html" "packages/portal/frontend/html/${envVar}/student.html"
-}
-
-function setEnvName() {
-	echo "default-file-setup.sh:: setEnvName() started"
-	declare lastLine
-	while read line; 
-		do 
-			## HACK because multiple NAME= strings exist in file
-			if [[ "${lastLine}" =~ "## Name of the org" ]]; 
-				then
-					envVar=$(echo "${line}" | cut -d"=" -f 2)
-					echo "default-file-setup.sh:: setEnvName() Set envVar to ${envVar}"
-				fi
-			lastLine=${line}
-	done < ${envFile}
-
-	if [ $envVar == '' ]; then
-		echo "default-file-setup.sh:: setEnvName() ERROR - COULD NOT FIND NAME PROPERTY IN CLASSY .env FILE. MAKE SURE IT IS BELOW '## Name of the org' LINE"
+for filename in "${customFiles[@]}"
+do
+	if [ -f "${filename}" ]; then
+		echo "default-file-setup.sh:: ERROR: File ${filename} exists!"
+		echo "default-file-setup.sh:: CANNOT CREATE CUSTOM FILES IF A CUSTOM FILE ALREADY EXISTS. ALL CUSTOM FILES MUST BE REMOVED BEFORE RE-CREATING CUSTOM FILES FROM DEFAULTS"
 		exit 1
 	fi
-}
+done
 
-main
+
+## 2. Create custom boilerplate files from default files
+## -n flag to not overwrite just to be safe
+
+echo "default-file-setup.sh:: starting to copy defaults to custom"
+
+## FRONTEND - HTML VIEWS
+# If mkdir fails, we want this to stop: if the parent doesn't exist, there's a problem...
+mkdir -v "packages/portal/frontend/html/${envVar}/"
+cp -nv "packages/portal/frontend/html/default/custom.html" "packages/portal/frontend/html/${envVar}/custom.html"
+cp -nv "packages/portal/frontend/html/default/landing.html" "packages/portal/frontend/html/${envVar}/landing.html"
+cp -nv "packages/portal/frontend/html/default/login.html" "packages/portal/frontend/html/${envVar}/login.html"
+cp -nv "packages/portal/frontend/html/default/student.html" "packages/portal/frontend/html/${envVar}/student.html"
+
+## FRONTEND - VIEW MODELS
+cp -nv "packages/portal/frontend/src/app/custom/DefaultStudentView.ts" "packages/portal/frontend/src/app/custom/CustomStudentView.ts"
+cp -nv "packages/portal/frontend/src/app/custom/DefaultAdminView.ts" "packages/portal/frontend/src/app/custom/CustomAdminView.ts"
+
+## BACKEND
+cp -nv "packages/portal/backend/src/custom/DefaultCourseRoutes.ts" "packages/portal/backend/src/custom/CustomCourseRoutes.ts"
+cp -nv "packages/portal/backend/src/custom/DefaultCourseController.ts" "packages/portal/backend/src/custom/CustomCourseController.ts"
+
+echo "default-file-setup.sh:: custom files successfully created from defaults"
