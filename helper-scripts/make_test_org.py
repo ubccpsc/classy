3fa99069952ae001c95b2608d3a01c34e26c314c
Andrew Stec <andrewstec@gmail.com>
2019-08-21 13:45:59 -0700

    Updated script to include required GH admin token to create repo, updated .sample.env

--- helper-scripts/make_test_org.py
+++ helper-scripts/make_test_org.py
@@ -6,6 +6,12 @@
 
 ## NOTE: Username teams are setup to work with CI test logic. Some atest-xx users are admins and staff in random 
 ## places. Ideally, the randomness should be cleaned up in Classy and updated in this script.
+
+## NOTE: Autobot MUST be added to ORG maintainer to have permissions to see all teams, repos, etc. We are using Autobot's GH Token.
+## Simplest rule to remember. 
+
+## INSTRUCTIONS: Run script AFTER modifying .env with testing/development environment information.
+
 import requests
 import time
 import json
@@ -17,16 +23,15 @@ from dotenv import load_dotenv
 
 load_dotenv('./.env')
 
-org_name = 'TEST-ORG'
-org_login = 'TEST-ORG'
-api_token = os.getenv('GH_BOT_TOKEN').split("token ",1)[1] 
+org_name = 'classytest'
+org_login = 'classytest'
+api_token = os.getenv('ORG_ADMIN_TOKEN')
 api_base_uri = os.getenv('GH_API')
 github_url = os.getenv('GH_HOST')
-classy_name = os.getenv('NAME')
 headers = {'Content-Type': 'application/json',
 			'Authorization': f'token {api_token}'}
-maintainer = 'steca'
-admin_team_members = ['atest-03', 'atest-09', 'autobot', 'classytest-admin', 'steca']
+maintainer = os.getenv('ORG_ADMIN_USER')
+admin_team_members = ['atest-03', 'atest-09', 'classytest-admin', 'steca', 'autobot']
 staff_team_members = ['atest-08', 'atest-09', 'classytest-admstaff', 'classytest-staff']
 students_team_members = ['atest-01', 'atest-02', 'atest-03', 'atest-04', 'atest-05', 'atest-06', 'atest-07', 'atest-08',
 	'atest-09', 'atest-10']
@@ -73,10 +78,13 @@ def handle_response(response, endpoint_url):
 		print(response.json())
 		print(endpoint_url + ' failed')
 
-def add_org_members(org_members):
+def add_org_members(org_members, role):
 	endpoint_url = api_base_uri + '/orgs/' + org_login + '/memberships/'
+	data = dict(
+		role=role
+		)
 	for member in org_members:
-		response = requests.put(endpoint_url + member, headers=headers)
+		response = requests.put(endpoint_url + member, json=data, headers=headers)
 		handle_response(response, endpoint_url)
 
 ## 1. Make Organization
@@ -87,31 +95,31 @@ def add_org_members(org_members):
 make_organization(org_name, maintainer)
 
 ## 2. Add team members to organization (REQUIRED before adding to teams)
-add_org_members(staff_team_members)
-add_org_members(admin_team_members)
-add_org_members(students_team_members)
+add_org_members(admin_team_members, 'admin')
+add_org_members(staff_team_members, 'member')
+add_org_members(students_team_members, 'member')
 
-# 3. Creates 'admin', 'staff', and 'students' teams
-create_team('staff', staff_team_members)
+## 3. Creates 'admin', 'staff', and 'students' teams
 create_team('admin', admin_team_members)
+create_team('staff', staff_team_members)
 create_team('students', students_team_members)
 
-# # ## 4. Create empty repos used in CI testing
+## 4. Create empty repos used in CI testing
 create_repo('PostTestDoNotDelete', 'Tests the AutoTest bot can comment code to a repository')
 create_repo('TESTING_SAMPLE_REPO', 'To test that the AutoTest bot can successfully clone a repo with its files')
 
-# # ## 5a. Copy base repo code to empty repos
-postTestBaseRepo = git.Repo.clone_from(github_url + '/classytest/PostTestDoNotDelete.git', os.path.join('./helper-scripts/', 'PostTestDoNotDelete'))
+## 5a. Copy base repo code to empty repos
+postTestBaseRepo = git.Repo.clone_from('https://github.students.cs.ubc.ca/classytest/PostTestDoNotDelete.git', os.path.join('./helper-scripts/', 'PostTestDoNotDelete'))
 postTestBaseRepo.delete_remote('origin')
 postTestBaseRepo.create_remote('origin', github_url + '/' + org_login + '/PostTestDoNotDelete.git')
 postTestBaseRepo.git.push('origin', 'master')
 
-cloneTestBaseRepo = git.Repo.clone_from(github_url + '/classytest/TESTING_SAMPLE_REPO.git', os.path.join('./helper-scripts/', 'TESTING_SAMPLE_REPO'))
+cloneTestBaseRepo = git.Repo.clone_from('https://github.students.cs.ubc.ca/classytest/TESTING_SAMPLE_REPO.git', os.path.join('./helper-scripts/', 'TESTING_SAMPLE_REPO'))
 cloneTestBaseRepo.delete_remote('origin')
 cloneTestBaseRepo.create_remote('origin', github_url + '/' + org_login + '/TESTING_SAMPLE_REPO.git')
 cloneTestBaseRepo.git.push('origin', 'master')
 
-# # ## 5b. clean-up filesystem repo cloning
+## 5b. clean-up filesystem repo cloning
 shutil.rmtree('./helper-scripts/PostTestDoNotDelete')
 shutil.rmtree('./helper-scripts/TESTING_SAMPLE_REPO')
 
