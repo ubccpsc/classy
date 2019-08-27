e4d5943729b59d2e57e97552a870d82804b39f56
Andrew Stec <andrewstec@gmail.com>
2019-08-27 08:47:04 -0700

    Adding MAKE_TEST_ORG readme instructions and business logic caveats into runtime prompt of make_test_org.py

--- helper-scripts/make_test_org.py
+++ helper-scripts/make_test_org.py
@@ -1,22 +1,14 @@
 ## ISSUES:
-## Can not re-run script. Org must be deleted and re-made to re-run
+## Can not re-run script. Org must be deleted and re-made to re-run if it only makes it part-way.
 
-## NOTE: Must choose a maintainer who is an admin on GH Enterprise on Enterprise, 
-## OR Organization owner on Github.com public website.
-
-## NOTE: Username teams are setup to work with CI test logic. Some atest-xx users are admins and staff in random 
-## places. Ideally, the randomness should be cleaned up in Classy and updated in this script.
-
-## NOTE: Autobot MUST be added to ORG maintainer to have permissions to see all teams, repos, etc. We are using Autobot's GH Token.
-## Simplest rule to remember. 
-
-## INSTRUCTIONS: Run script AFTER modifying .env with testing/development environment information.
+## INSTRUCTIONS: Run script AFTER entering GH_HOST and GH_API in .env file
 
 import requests
 import time
 import json
 import git
 import os
+import getpass
 import shutil
 from pprint import pprint
 from dotenv import load_dotenv
@@ -24,62 +16,69 @@ from dotenv import load_dotenv
 load_dotenv('./.env')
 
 org_name = 'classytest'
-org_login = 'classytest'
-api_token = os.getenv('ORG_ADMIN_TOKEN')
+api_token = ''
 api_base_uri = os.getenv('GH_API')
+github_enterprise = False
 github_url = os.getenv('GH_HOST')
 headers = {'Content-Type': 'application/json',
 			'Authorization': f'token {api_token}'}
-maintainer = os.getenv('ORG_ADMIN_USER')
-admin_team_members = ['classytest-admstaff', 'classytest-admin', 'steca', 'autobot']
+maintainer = ''
+admin_team_members = ['classytest-admstaff', 'classytest-admin', 'classytest-bot01', 'classytest-bot02']
 staff_team_members = ['classytest-admstaff', 'classytest-staff']
-students_team_members = ['atest-01', 'atest-02', 'atest-03', 'atest-04', 'atest-05', 'atest-06', 'atest-07', 'atest-08',
-	'atest-09']
+students_team_members = ['atest-04', 'atest-05', 'atest-06', 'atest-07', 'atest-08', 'atest-09', 'atest-10']
+
+def get_y_or_n(question):
+	answer = ''
+	while answer != 'y' and answer != 'n':
+		answer = input(question)
+	if answer == 'y':
+		return True
+	if answer == 'n':
+		return False
 
 def make_organization(org_name, maintainer):
 	endpoint_url = api_base_uri + '/admin/organizations';
 	data = dict(
-		login=org_login,
+		login=org_name,
 		admin=maintainer,
 		profile_name=org_name
 		)
-	response = requests.post(endpoint_url, json=data, headers=headers)
-	handle_response(response, endpoint_url)
+	res = requests.post(endpoint_url, json=data, headers=headers)
+	handle_response(res, endpoint_url)
 
 
 def create_repo(name, description):
-	endpoint_url = api_base_uri + '/orgs/' + org_login + '/repos'
+	endpoint_url = api_base_uri + '/orgs/' + org_name + '/repos'
 	data = {
 		'name': name,
 		'description': description,
 		'private': False
 	}
-	response = requests.post(endpoint_url, json=data, headers=headers)
-	handle_response(response, endpoint_url)
+	res = requests.post(endpoint_url, json=data, headers=headers)
+	handle_response(res, endpoint_url)
 
 def create_team(team_name, team_members):
-	endpoint_url = api_base_uri + '/orgs/' + org_login + '/teams'
+	endpoint_url = api_base_uri + '/orgs/' + org_name + '/teams'
 	print(team_members)
 	data = dict(
 		name=team_name,
 		maintainers=team_members,
 		permission='pull'
 		)
-	response = requests.post(endpoint_url, json=data, headers=headers)
-	handle_response(response, endpoint_url)
+	res = requests.post(endpoint_url, json=data, headers=headers)
+	handle_response(res, endpoint_url)
 
-def handle_response(response, endpoint_url):
-	if response.status_code >= 200 and response.status_code < 300:
-		print(response.status_code)
-		print(response.json())
-		print(endpoint_url + ' succeeded')
-	else: 
-		print(response.status_code)
-		print(response.json())
-		print(endpoint_url + ' failed')
+def handle_response(res, endpoint_url):
+	if res.status_code >= 200 and res.status_code < 300:
+		print(res.status_code)
+		print('SUCCESS: ' + str(res.status_code) + ' on ' + endpoint_url)
+	else:
+		print('FAILURE: ' + str(res.status_code) + ' on ' + endpoint_url)
+		print(res.json())
+		exit(1)
 
 def add_org_members(org_members, role):
-	endpoint_url = api_base_uri + '/orgs/' + org_login + '/memberships/'
+	endpoint_url = api_base_uri + '/orgs/' + org_name + '/memberships/'
 	data = dict(
 		role=role
 		)
@@ -87,19 +86,38 @@ def add_org_members(org_members, role):
 		response = requests.put(endpoint_url + member, json=data, headers=headers)
 		handle_response(response, endpoint_url)
 
-## 1. Make Organization
-##
-## NOTE: ONLY WORKS ON GH ENTERPRISE
-## CANNOT make orgnaization VIA API on public Github.com (create manually and comment out step #1)
-##
-make_organization(org_name, maintainer)
 
-## 2. Add team members to organization (REQUIRED before adding to teams)
+## PRE SETUP - Setup vars, warnings
+print('''This script will setup a `classytest` organization for CI testing. 
+	IMPORTANT: Github.com public requires that you manually create a `classytest` organization and enter an API key with owner permissions.
+	Github Enterprise will create the `classytest` organization automatically. This requires that you enter an API key with Github Admin permissions''')
+print('To proceed, enter your organization owner API key Github Admin permissions: ')
+api_token = getpass.getpass()
+github_enterprise = get_y_or_n('Are you using a Github Enterprise instance? (y/n): ')
+
+## 1. Make Organization
+if github_enterprise: 
+	maintainer = input('Enter your organization maintainer username: ').lower().strip()
+	make_organization(org_name, maintainer)
+
+## 2. Add users to organization
+if github_enterprise == False:
+	proceed = get_y_or_n('''Github.com public REQUIRES that team members accept an organization invite before being added to the team. Do you want to proceed with
+		inviting admin, staff, and students testing accounts to ''' + org_name + ' on ' + github_url + '? (y/n): ')
+	if proceed == False:
+		exit()
+
 add_org_members(admin_team_members, 'admin')
 add_org_members(staff_team_members, 'member')
 add_org_members(students_team_members, 'member')
 
-## 3. Creates 'admin', 'staff', and 'students' teams
+## 3. Add users to teams
+if github_enterprise == False: 
+	proceed = get_y_or_no('Github users have been invited to your organization. Before you can proceed to add users to a team, they MUST accept your invitiation. ' +
+			'Do you want to proceed with adding users to the admin, staff, and students teams? (y/n):') 
+	if proceed == False:
+		exit()
+
 create_team('admin', admin_team_members)
 create_team('staff', staff_team_members)
 create_team('students', students_team_members)
@@ -108,20 +126,22 @@ create_team('students', students_team_members)
 create_repo('PostTestDoNotDelete', 'Tests the AutoTest bot can comment code to a repository')
 create_repo('TESTING_SAMPLE_REPO', 'To test that the AutoTest bot can successfully clone a repo with its files')
 
-## 5a. Copy base repo code to empty repos
+## 5. Copy base repo code to empty repos
 postTestBaseRepo = git.Repo.clone_from('https://github.students.cs.ubc.ca/classytest/PostTestDoNotDelete.git', os.path.join('./helper-scripts/', 'PostTestDoNotDelete'))
 postTestBaseRepo.delete_remote('origin')
-postTestBaseRepo.create_remote('origin', github_url + '/' + org_login + '/PostTestDoNotDelete.git')
+postTestBaseRepo.create_remote('origin', github_url + '/' + org_name + '/PostTestDoNotDelete.git')
 postTestBaseRepo.git.push('origin', 'master')
 
 cloneTestBaseRepo = git.Repo.clone_from('https://github.students.cs.ubc.ca/classytest/TESTING_SAMPLE_REPO.git', os.path.join('./helper-scripts/', 'TESTING_SAMPLE_REPO'))
 cloneTestBaseRepo.delete_remote('origin')
-cloneTestBaseRepo.create_remote('origin', github_url + '/' + org_login + '/TESTING_SAMPLE_REPO.git')
+cloneTestBaseRepo.create_remote('origin', github_url + '/' + org_name + '/TESTING_SAMPLE_REPO.git')
 cloneTestBaseRepo.git.push('origin', 'master')
 
-## 5b. clean-up filesystem repo cloning
+## POST: clean-up repo cloning on filesystem
 shutil.rmtree('./helper-scripts/PostTestDoNotDelete')
 shutil.rmtree('./helper-scripts/TESTING_SAMPLE_REPO')
 
+print('Organization setup complete')
+exit()
 
 
