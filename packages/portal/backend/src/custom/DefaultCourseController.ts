ffc0404494cb47829bdff69ec1100373724a9d8f
Reid Holmes (MBA) <rtholmes@gmail.com>
2019-09-13 12:37:07 -0700

    Get stub in where team numbers should be worked out

--- packages/portal/backend/src/custom/DefaultCourseController.ts
+++ packages/portal/backend/src/custom/DefaultCourseController.ts
@@ -1,6 +1,7 @@
 import Log from "../../../../common/Log";
 import {CourseController} from "../controllers/CourseController";
 import {IGitHubController} from "../controllers/GitHubController";
+import {Deliverable, Person} from "../Types";
 
 export class DefaultCourseController extends CourseController {
 
@@ -8,4 +9,10 @@ export class DefaultCourseController extends CourseController {
         Log.trace("DefaultCourseController::<init>");
         super(ghController);
     }
+
+    public async computeNames(deliv: Deliverable, people: Person[]): Promise<string> {
+        Log.info("DefaultCourseController::computeNames() - passing up to CourseController");
+        return super.computeNames(deliv, people);
+    }
+
 }
