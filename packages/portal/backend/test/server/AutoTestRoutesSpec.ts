643b8634e50277cfd4637bb26bd15b7b051f82f7
Andrew Stec <andrewstec@gmail.com>
2019-10-29 13:44:56 -0700

    Bringing test back in as a graceful fail is needed, but modifying it for this edge case

--- packages/portal/backend/test/server/AutoTestRoutesSpec.ts
+++ packages/portal/backend/test/server/AutoTestRoutesSpec.ts
@@ -440,26 +440,31 @@ describe('AutoTest Routes', function() {
         expect(response.body.failure.message).to.be.a('string');
     });
 
-    // // this will always fail now that we check the IP of the host
-    // it('Should be able to receive a Webhook event from GitHub, but fail gracefully.', async function() {
-    //     // NOTE: this is a terrible tests; without the service running we get nothing
-    //     let response = null;
-    //     const body = fs.readJSONSync(__dirname + "/../../../../autotest/test/githubEvents/push_master-branch.json"); // __dirname
+    // this will always fail now that we check the IP of the host
+    it('Should be able to receive a Webhook event from GitHub, but fail gracefully.', async function() {
+        // NOTE: this is a terrible tests; without the service running we get nothing
+        let response = null;
+        const body = fs.readJSONSync(__dirname + "/../../../../autotest/test/githubEvents/push_master-branch.json"); // __dirname
+        const autotestUrl = Config.getInstance().getProp(ConfigKey.autotestUrl);
 
-    //     const url = '/portal/githubWebhook';
-    //     try {
-    //         response = await request(app)
-    //             .post(url).send(body)
-    //             .set('Accept', 'application/json')
-    //             .set('token', Config.getInstance().getProp(ConfigKey.autotestSecret));
-    //     } catch (err) {
-    //         Log.test('ERROR: ' + err);
-    //     }
-    //     Log.test(response.status + " -> " + JSON.stringify(response.body));
-    //     expect(response.status).to.equal(400); // really should be 200, but AutoTest isn't running so it will return this error
-    //     const text = response.text;
-    //     expect(text.indexOf('ECONNREFUSED')).to.be.greaterThan(0); // at least make sure it fails for the right reason
-    // });
+        const url = '/portal/githubWebhook';
+        try {
+            response = await request(app)
+                .post(url).send(body)
+                .set('Accept', 'application/json')
+                .set('token', Config.getInstance().getProp(ConfigKey.autotestSecret));
+        } catch (err) {
+            Log.test('ERROR: ' + err);
+        }
+        Log.test(response.status + " -> " + JSON.stringify(response.body));
+        expect(response.status).to.equal(400); // really should be 200, but AutoTest isn't running so it will return this error
+        const text = response.text;
+        if (autotestUrl.indexOf('localhost') > -1) {
+            expect(text.indexOf('ECONNREFUSED')).to.be.greaterThan(0); // at least make sure it fails for the right reason
+        } else {
+            expect(text.indexOf('ENOTFOUND')).to.be.greaterThan(0); // non-localhost autotest hostname name results in different error
+        }
+    });
 
     describe('GET /portal/at/docker/images', function() {
         const url = '/portal/at/docker/images';
