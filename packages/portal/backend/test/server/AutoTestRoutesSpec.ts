b204c3d2a3917be83efc271d5dfc75f7a8bc7c03
Andrew Stec <andrewstec@gmail.com>
2019-10-29 11:53:04 -0700

    This test does not work, and we should consider taking it out

--- packages/portal/backend/test/server/AutoTestRoutesSpec.ts
+++ packages/portal/backend/test/server/AutoTestRoutesSpec.ts
@@ -440,26 +440,26 @@ describe('AutoTest Routes', function() {
         expect(response.body.failure.message).to.be.a('string');
     });
 
-    // this will always fail now that we check the IP of the host
-    it('Should be able to receive a Webhook event from GitHub, but fail gracefully.', async function() {
-        // NOTE: this is a terrible tests; without the service running we get nothing
-        let response = null;
-        const body = fs.readJSONSync(__dirname + "/../../../../autotest/test/githubEvents/push_master-branch.json"); // __dirname
+    // // this will always fail now that we check the IP of the host
+    // it('Should be able to receive a Webhook event from GitHub, but fail gracefully.', async function() {
+    //     // NOTE: this is a terrible tests; without the service running we get nothing
+    //     let response = null;
+    //     const body = fs.readJSONSync(__dirname + "/../../../../autotest/test/githubEvents/push_master-branch.json"); // __dirname
 
-        const url = '/portal/githubWebhook';
-        try {
-            response = await request(app)
-                .post(url).send(body)
-                .set('Accept', 'application/json')
-                .set('token', Config.getInstance().getProp(ConfigKey.autotestSecret));
-        } catch (err) {
-            Log.test('ERROR: ' + err);
-        }
-        Log.test(response.status + " -> " + JSON.stringify(response.body));
-        expect(response.status).to.equal(400); // really should be 200, but AutoTest isn't running so it will return this error
-        const text = response.text;
-        expect(text.indexOf('ECONNREFUSED')).to.be.greaterThan(0); // at least make sure it fails for the right reason
-    });
+    //     const url = '/portal/githubWebhook';
+    //     try {
+    //         response = await request(app)
+    //             .post(url).send(body)
+    //             .set('Accept', 'application/json')
+    //             .set('token', Config.getInstance().getProp(ConfigKey.autotestSecret));
+    //     } catch (err) {
+    //         Log.test('ERROR: ' + err);
+    //     }
+    //     Log.test(response.status + " -> " + JSON.stringify(response.body));
+    //     expect(response.status).to.equal(400); // really should be 200, but AutoTest isn't running so it will return this error
+    //     const text = response.text;
+    //     expect(text.indexOf('ECONNREFUSED')).to.be.greaterThan(0); // at least make sure it fails for the right reason
+    // });
 
     describe('GET /portal/at/docker/images', function() {
         const url = '/portal/at/docker/images';
