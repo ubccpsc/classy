7d66ba69d73a57eaf17922972fd6121fcd8fd014
Andrew Stec <andrewstec@gmail.com>
2019-11-14 12:27:32 -0800

    Implemented interactive input in script

--- helper-scripts/data_export_json.sh
+++ helper-scripts/data_export_json.sh
@@ -5,7 +5,6 @@
 ##
 ## Example usage: ./data_export_json.sh output.json results "{ delivId: 'd2' }"
 
-
 ## $1 - filename to export json to
 ## $2 - table to export
 ## $3 - optional query parameters in string ie. "{ delivId: 'd2' }"
@@ -14,29 +13,36 @@ user=`grep MONGO_INITDB_ROOT_USERNAME /opt/classy/.env | sed -e 's/^MONGO_INITDB
 pw=`grep MONGO_INITDB_ROOT_PASSWORD /opt/classy/.env | sed -e 's/^MONGO_INITDB_ROOT_PASSWORD=//'`
 database=`grep NAME /opt/classy/.env -m 1 | sed -e 's/^NAME=//'`
 
-# user=`grep MONGO_INITDB_ROOT_USERNAME ./.env | sed -e 's/^MONGO_INITDB_ROOT_USERNAME=//'`
-# pw=`grep MONGO_INITDB_ROOT_PASSWORD ./.env | sed -e 's/^MONGO_INITDB_ROOT_PASSWORD=//'`
-# database=`grep MONGO_INITDB_ROOT_PASSWORD ./.env | sed -e 's/^NAME=//'`
-
 outputPath="$1"
 table="$2"
-query="--query=$3"
+query="$3"
+
+echo "### Data Exporter v1"
+echo "# This script exports data JSON data from MongoDB to a JSON file. By default, the `results` table of
+a course will be exported to your current directory."
+
+while true; do
+    read -p "Do you want to continue with data export operation? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer 'yes' or 'no': ";;
+    esac
+done
 
 if [ -z $1 ]
   then
-    echo "ERROR: Must enter an output path. ie. ./results.json"
-    exit 1
+    read -p "Enter destination file path or press 'Enter' to default to ~/results.json: " outputPath
 fi
 
 if [ -z $2 ]
   then
-    echo "WARNING: No table selected. Defaulting to export 'results' table..."
-    table='results'
+    read -p "What table would you like to export from MongoDB ${database} database? : " table
 fi
 
 if [ -z $3 ]
   then
-    echo "No query arguments supplied. Defaulting to entire table export."
+    read -p "Please enter an optional query. Your query must be a valid MongoDB query in JSON format (ie. '{ delivId: 'd2' }'): " query
 fi
 
-docker exec db mongoexport --username="$user" --password="$pw" --db="$database" --collection="$table" "$query" --authenticationDatabase=admin --jsonArray > "$outputPath"
+docker exec db mongoexport --username="$user" --password="$pw" --db="$database" --collection="$table" --query="$query" --authenticationDatabase=admin --jsonArray > "$outputPath"
