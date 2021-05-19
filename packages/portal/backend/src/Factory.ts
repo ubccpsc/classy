cd9be02af9b76f2e3a999b96cb9748d9e47cdce9
Andrew Stec <andrewstec@gmail.com>
2021-05-19 07:52:59 -0700

    Updating to plugin path specified in docs

--- packages/portal/backend/src/Factory.ts
+++ packages/portal/backend/src/Factory.ts
@@ -40,7 +40,7 @@ export class Factory {
             let plug: any;
             if (pluginPath) {
                 Log.info("Factory::getCustomRouteHandler() - instantiating CustomCourseRoutes for: " + name + "; path: " + pluginPath);
-                plug = await require("../../../../plugin/portal/backend/src/CustomCourseRoutes"); // default for testing
+                plug = await require("../../../../plugin/portal/backend/CustomCourseRoutes"); // default for testing
             } else {
                 // If a course wants to specialize the AdminView it should be in the file below.
                 // This is not required. But if it is added, it should never be pushed back to 'classy/master'
@@ -103,7 +103,7 @@ export class Factory {
                     // If a course wants to specialize the AdminView it should be in the file below.
                     // This is not required. But if it is added, it should never be pushed back to 'classy/master'
                     Log.trace("Factory::getCourseController() - name: " + name + " - plug: CustomCourseController");
-                    plug = await require("../../../../plugin/portal/backend/src/CustomCourseController");
+                    plug = await require("../../../../plugin/portal/backend/CustomCourseController");
                 } else {
                     Log.trace("Factory::getCourseController() - name: " + name + " - plug: DefaultCourseController");
                     plug = await require("./custom/DefaultCourseController");
