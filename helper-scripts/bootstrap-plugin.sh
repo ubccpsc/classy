aa96e633c7c505598d3ccb2ab8d2a990d8467418
Andrew Stec <andrewstec@gmail.com>
2021-06-04 11:27:13 -0700

    Debugging

--- helper-scripts/bootstrap-plugin.sh
+++ helper-scripts/bootstrap-plugin.sh
@@ -5,11 +5,7 @@
 # during the Docker build, but not before, in a production environment.
 
 # Run from root Classy project dir ie. /opt/classy/
-workdir=`pwd`
-echo "Working Dir: $workdir"
-ls -lh
-echo `awk -F = '/^PLUGIN[[:space:]]*=/{gsub(/[[:space:]]/, "", $2); print $2}' ./.env`
-tail .env
+awk --version
 
 ./helper-scripts/set-docker-override.sh
 
