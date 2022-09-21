8c4b1004aba0fbd5f7939764c239438a5f572b66
Reid Holmes <rtholmes@gmail.com>
2022-09-20 21:02:36 -0700

    add chai-as-promised

--- packages/portal/backend/test/FactorySpec.ts
+++ packages/portal/backend/test/FactorySpec.ts
@@ -1,17 +1,22 @@
-import {expect} from "chai";
+// import { use as chaiUse } from 'chai';
+import {expect, use as chaiUse} from "chai";
+import * as chaiAsPromised from "chai-as-promised";
 import "mocha";
 import Log from "../../../common/Log";
 
 import {Factory} from "../src/Factory";
 
 import './GlobalSpec';
+import Config, {ConfigKey} from "@common/Config";
 
-describe('Factory', function() {
+chaiUse(chaiAsPromised);
+
+describe('Factory', function () {
 
     /**
      * These are all terrible tests and just make sure that _some_ object is returned.
      */
-    it('Can get the route handler for courses', async function() {
+    it('Can get the route handler for courses', async function () {
         let actual = Factory.getCustomRouteHandler('classytest');
         expect(actual).to.not.be.null;
 
@@ -35,36 +40,26 @@ describe('Factory', function() {
         expect(ex).to.be.null;
     });
 
-    it('Can get the course controller for courses', async function() {
+    it('Can get the course controller for courses', async function () {
         // should be able to get our test controller
         const actual = await Factory.getCourseController(null, 'classytest');
         Log.test("Controller should not be null: " + actual);
         expect(actual).to.not.be.null;
+    });
 
-        // NOTE: this behaviour is different now: we just return the CustomCourseController no matter what.
+    it('Invalid plugins should be handled gracefully', async function () {
+        const pluginVal = Config.getInstance().getProp(ConfigKey.plugin);
+        Config.getInstance().setProp(ConfigKey.plugin, "INVALIDPLUGIN");
 
-        // should fail to get a controller for a course that doesn't exist
-        // actual = null;
-        // let ex = null;
-        // try {
-        //     actual = await Factory.getCourseController(null, 'INVALIDcourseNAME');
-        //     Log.test("Controller should be null: " + actual);
-        // } catch (err) {
-        //     ex = err;
-        // }
-        // expect(actual).to.be.null;
-        // expect(ex).to.not.be.null;
+        Log.test("1");
+        await expect(Factory.getCustomRouteHandler('INVALID_PLUGIN')).to.eventually.throw;
+        Log.test("2");
+        await expect(Factory.getCourseController(null, 'INVALID_PLUGIN')).to.eventually.throw;
+        Log.test("3");
+        await expect(Factory.getCourseController(undefined, undefined)).to.eventually.throw;
+        Log.test("4");
 
-        // actual = null;
-        // ex = null;
-        // try {
-        //     // won't error because it uses the default name
-        //     actual = Factory.getCourseController();
-        // } catch (err) {
-        //     ex = err;
-        // }
-        // expect(actual).to.be.null;
-        // expect(ex).to.not.be.null;
+        Config.getInstance().setProp(ConfigKey.plugin, pluginVal);
     });
 
 });
