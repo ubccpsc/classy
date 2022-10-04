034375dffb914a2a267315cb5f4bc92708825a60
Reid Holmes <rtholmes@gmail.com>
2022-10-03 19:55:28 -0700

    improve dropdown usability

--- packages/portal/frontend/src/app/views/AdminResultsTab.ts
+++ packages/portal/frontend/src/app/views/AdminResultsTab.ts
@@ -32,28 +32,28 @@ export class AdminResultsTab extends AdminPage {
 
     // called by reflection in renderPage
     public async init(opts: any): Promise<void> {
-        Log.info('AdminResultsTab::init(..) - start');
+        Log.info("AdminResultsTab::init(..) - start");
         const that = this;
 
         // NOTE: this could consider if studentListTable has children, and if they do, don't refresh
-        document.getElementById('resultsListTable').innerHTML = ''; // clear target
+        document.getElementById("resultsListTable").innerHTML = ""; // clear target
 
-        UI.showModal('Retrieving results.');
+        UI.showModal("Retrieving results.");
         const course = await AdminView.getCourse(this.remote);
         if (this.delivValue === null) {
             this.delivValue = course.defaultDeliverableId;
             // ugly way to set the default the first time the page is rendered
-            UI.setDropdownOptions('resultsDelivSelect', [this.delivValue], this.delivValue);
+            UI.setDropdownOptions("resultsDelivSelect", [this.delivValue], this.delivValue);
         }
         const delivs = await AdminDeliverablesTab.getDeliverables(this.remote); // for select
         const repos = await AdminResultsTab.getRepositories(this.remote); // for select
         const results = await this.performQueries();
         UI.hideModal();
 
-        const fab = document.querySelector('#resultsUpdateButton') as OnsButtonElement;
+        const fab = document.querySelector("#resultsUpdateButton") as OnsButtonElement;
         fab.onclick = function (evt: any) {
-            Log.info('AdminResultsTab::init(..)::updateButton::onClick');
-            UI.showModal('Retrieving results.');
+            Log.info("AdminResultsTab::init(..)::updateButton::onClick");
+            UI.showModal("Retrieving results.");
             that.performQueries().then(function (newResults) {
                 // TODO: need to track and update the current value of deliv and repo
                 that.render(delivs, repos, newResults);
@@ -67,97 +67,78 @@ export class AdminResultsTab extends AdminPage {
     }
 
     private async performQueries(): Promise<AutoTestResultSummaryTransport[]> {
-        Log.info('AdminResultsTab::performQueries(..) - start');
+        Log.info("AdminResultsTab::performQueries(..) - start");
         const start = Date.now();
-        let deliv = UI.getDropdownValue('resultsDelivSelect');
-        if (deliv === '-Any-') {
-            deliv = 'any';
+        let deliv = UI.getDropdownValue("resultsDelivSelect");
+        if (deliv === "-Any-") {
+            deliv = "any";
         }
-        let repo = UI.getDropdownValue('resultsRepoSelect');
-        if (repo === '-Any-') {
-            repo = 'any';
+        let repo = UI.getDropdownValue("resultsRepoSelect");
+        if (repo === "-Any-") {
+            repo = "any";
         }
 
         this.delivValue = deliv;
         this.repoValue = repo;
         const values = await AdminResultsTab.getResults(this.remote, this.delivValue, this.repoValue);
-        Log.info('AdminResultsTab::performQueries(..) - done; # values: ' + values.length + "; took: " + UI.took(start));
+        Log.info("AdminResultsTab::performQueries(..) - done; # values: " + values.length + "; took: " + UI.took(start));
         return values;
     }
 
-    private render(delivs: DeliverableTransport[],
-                   repos: RepositoryTransport[],
-                   results: AutoTestResultSummaryTransport[]): void {
-        Log.trace("AdminResultsTab::render(..) - start");
-
-        let delivNames: string[] = [];
-        for (const deliv of delivs) {
-            delivNames.push(deliv.id);
-        }
-        delivNames = delivNames.sort();
-        delivNames.unshift('-Any-');
-        UI.setDropdownOptions('resultsDelivSelect', delivNames, this.delivValue);
-
-        let repoNames: string[] = [];
-        for (const repo of repos) {
-            repoNames.push(repo.id);
-        }
-        repoNames = repoNames.sort();
-        repoNames.unshift('-Any-');
-        UI.setDropdownOptions('resultsRepoSelect', repoNames, this.repoValue);
-
+    private renderResults(results: AutoTestResultSummaryTransport[]): void {
+        Log.trace("AdminResultsTab::renderResults: " + results.length);
         const headers: TableHeader[] = [
             {
-                id: '?',
-                text: '?',
+                id: "?",
+                text: "?",
                 sortable: false,
                 defaultSort: false,
                 sortDown: true,
-                style: 'padding-left: 1em; padding-right: 1em; text-align: center;'
+                style: "padding-left: 1em; padding-right: 1em; text-align: center;"
             },
             {
-                id: 'repoId',
-                text: 'Repository',
+                id: "repoId",
+                text: "Repository",
                 sortable: true,
                 defaultSort: false,
                 sortDown: true,
-                style: 'padding-left: 1em; padding-right: 1em; text-align: left;'
+                style: "padding-left: 1em; padding-right: 1em; text-align: left;"
             },
             {
-                id: 'delivId',
-                text: 'Deliv',
+                id: "delivId",
+                text: "Deliv",
                 sortable: true, // Whether the column is sortable (sometimes sorting does not make sense).
                 defaultSort: false, // Whether the column is the default sort for the table. should only be true for one column.
                 sortDown: false, // Whether the column should initially sort descending or ascending.
-                style: 'padding-left: 1em; padding-right: 1em; text-align: center;'
+                style: "padding-left: 1em; padding-right: 1em; text-align: center;"
             },
             {
-                id: 'score',
-                text: 'Score',
+                id: "score",
+                text: "Score",
                 sortable: true,
                 defaultSort: false,
                 sortDown: true,
-                style: 'padding-left: 1em; padding-right: 1em; text-align: center;'
+                style: "padding-left: 1em; padding-right: 1em; text-align: center;"
             },
             {
-                id: 'state',
-                text: 'State',
+                id: "state",
+                text: "State",
                 sortable: true,
                 defaultSort: false,
                 sortDown: true,
-                style: 'padding-left: 1em; padding-right: 1em; text-align: center;'
+                style: "padding-left: 1em; padding-right: 1em; text-align: center;"
             },
             {
-                id: 'timstamp',
-                text: 'Timestamp',
+                id: "timstamp",
+                text: "Timestamp",
                 sortable: true,
                 defaultSort: true,
                 sortDown: true,
-                style: 'padding-left: 1em; padding-right: 1em; text-align: center;'
+                style: "padding-left: 1em; padding-right: 1em; text-align: center;"
             }
         ];
 
-        const st = new SortableTable(headers, '#resultsListTable');
+        const st = new SortableTable(headers, "#resultsListTable");
 
         // this loop couldn't possibly be less efficient
         for (const result of results) {
@@ -180,10 +161,10 @@ export class AdminResultsTab extends AdminPage {
             const tsString = mom.format("MM/DD[@]HH:mm");
             // const tsString = new Date(ts).toLocaleDateString() + ' @ ' + new Date(ts).toLocaleTimeString();
 
-            const stdioViewerURL = '/stdio.html?delivId=' + result.delivId + '&repoId=' + result.repoId + '&sha=' + result.commitSHA;
+            const stdioViewerURL = "/stdio.html?delivId=" + result.delivId + "&repoId=" + result.repoId + "&sha=" + result.commitSHA;
 
-            let score: number | string = '';
-            let scorePrepend = '';
+            let score: number | string = "";
+            let scorePrepend = "";
             score = result.scoreOverall;
             if (score === 100) {
                 score = "100.00";
@@ -203,7 +184,7 @@ export class AdminResultsTab extends AdminPage {
             // ion-ios-help-outline
             const row: TableCell[] = [
                 {
-                    value: '',
+                    value: "",
                     html: '<a style="cursor: pointer; cursor: hand;" target="_blank" href="' +
                         stdioViewerURL + '"><ons-icon icon="md-info-outline"</ons-icon></a>'
                 },
@@ -223,18 +204,63 @@ export class AdminResultsTab extends AdminPage {
 
         st.generate();
 
+        if (st.numRows() > 0) {
+            UI.showSection("resultsListTable");
+            UI.hideSection("resultsListTableNone");
+        } else {
+            UI.showSection("resultsListTable");
+            UI.hideSection("resultsListTableNone");
+        }
+    }
+
+    private render(delivs: DeliverableTransport[],
+                   repos: RepositoryTransport[],
+                   results: AutoTestResultSummaryTransport[]): void {
+        Log.trace("AdminResultsTab::render(..) - start");
+
+        const that = this;
+
+        let delivNames: string[] = [];
+        for (const deliv of delivs) {
+            delivNames.push(deliv.id);
+        }
+        delivNames = delivNames.sort();
+        delivNames.unshift("-Any-");
+        UI.setDropdownOptions("resultsDelivSelect", delivNames, this.delivValue);
+
+        let repoNames: string[] = [];
+        for (const repo of repos) {
+            repoNames.push(repo.id);
+        }
+        repoNames = repoNames.sort();
+        repoNames.unshift("-Any-");
+        UI.setDropdownOptions("resultsRepoSelect", repoNames, this.repoValue);
+
+        this.renderResults(results);
+
         try {
-            new TomSelect("#resultsRepoSelect", {maxOptions: null, maxItems: 1});
+            new TomSelect("#resultsRepoSelect", {
+                maxOptions: null,
+                maxItems: 1,
+                closeAfterSelect: true,
+                onDropdownOpen: function () {
+                    Log.trace("AdminResultsTab::render(..)::repoSelect - Clearing input: " + this);
+                    this.setValue("");
+                },
+                onDropdownClose: function () {
+                    // heavyweight way to initiate a search, but it works
+                    Log.trace("AdminResultsTab::render(..)::repoSelect - Performing search: " + this);
+                    Log.trace("AdminResultsTab::render(..)::repoSelect - Search value: " + this.getValue());
+                    if (this.getValue() === "") {
+                        // if nothing selected, go back to any
+                        this.setValue("-Any-");
+                    }
+                    that.init({}).then().catch(); // promises ignored
+                    this.blur();
+                }
+            });
         } catch (err) {
             Log.trace("AdminResultsTab::render(..) - updating select; MSG: " + err.message);
-        }
-
-        if (st.numRows() > 0) {
-            UI.showSection('resultsListTable');
-            UI.hideSection('resultsListTableNone');
-        } else {
-            UI.showSection('resultsListTable');
-            UI.hideSection('resultsListTableNone');
         }
     }
 
@@ -242,23 +268,22 @@ export class AdminResultsTab extends AdminPage {
         Log.info("AdminResultsTab::getResults( .. ) - start");
 
         const start = Date.now();
-        const url = remote + '/portal/admin/results/' + delivId + '/' + repoId;
+        const url = remote + "/portal/admin/results/" + delivId + "/" + repoId;
         const options = AdminView.getOptions();
         const response = await fetch(url, options);
 
         if (response.status === 200) {
-            Log.trace('AdminResultsTab::getResults(..) - 200 received');
+            Log.trace("AdminResultsTab::getResults(..) - 200 received");
             const json: AutoTestResultSummaryPayload = await response.json();
-            // Log.trace('AdminView::handleStudents(..)  - payload: ' + JSON.stringify(json));
-            if (typeof json.success !== 'undefined' && Array.isArray(json.success)) {
-                Log.trace('AdminResultsTab::getResults(..)  - worked; # rows: ' + json.success.length + '; took: ' + UI.took(start));
+            if (typeof json.success !== "undefined" && Array.isArray(json.success)) {
+                Log.trace("AdminResultsTab::getResults(..)  - worked; # rows: " + json.success.length + "; took: " + UI.took(start));
                 return json.success;
             } else {
-                Log.trace('AdminResultsTab::getResults(..)  - ERROR: ' + json.failure.message);
+                Log.trace("AdminResultsTab::getResults(..)  - ERROR: " + json.failure.message);
                 AdminView.showError(json.failure); // FailurePayload
             }
         } else {
-            Log.trace('AdminResultsTab::getResults(..)  - !200 received: ' + response.status);
+            Log.trace("AdminResultsTab::getResults(..)  - !200 received: " + response.status);
             const text = await response.text();
             AdminView.showError(text);
         }
@@ -271,24 +296,23 @@ export class AdminResultsTab extends AdminPage {
 
         try {
             const start = Date.now();
-            const url = remote + '/portal/admin/repositories';
+            const url = remote + "/portal/admin/repositories";
             const options = AdminView.getOptions();
             const response = await fetch(url, options);
 
             if (response.status === 200) {
-                Log.trace('AdminResultsTab::getRepositories(..) - 200 received');
+                Log.trace("AdminResultsTab::getRepositories(..) - 200 received");
                 const json: RepositoryPayload = await response.json();
-                // Log.trace('AdminView::handleStudents(..)  - payload: ' + JSON.stringify(json));
-                if (typeof json.success !== 'undefined' && Array.isArray(json.success)) {
-                    Log.trace('AdminResultsTab::getRepositories(..)  - worked; # repos: ' +
-                        json.success.length + '; took: ' + UI.took(start));
+                if (typeof json.success !== "undefined" && Array.isArray(json.success)) {
+                    Log.trace("AdminResultsTab::getRepositories(..)  - worked; # repos: " +
+                        json.success.length + "; took: " + UI.took(start));
                     return json.success;
                 } else {
-                    Log.trace('AdminResultsTab::getRepositories(..)  - ERROR: ' + json.failure.message);
+                    Log.trace("AdminResultsTab::getRepositories(..)  - ERROR: " + json.failure.message);
                     AdminView.showError(json.failure); // FailurePayload
                 }
             } else {
-                Log.trace('AdminResultsTab::getRepositories(..)  - !200 received: ' + response.status);
+                Log.trace("AdminResultsTab::getRepositories(..)  - !200 received: " + response.status);
                 const text = await response.text();
                 AdminView.showError(text);
             }
