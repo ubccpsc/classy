02b932a041ebf46a951381d3340e0266d72d4693
Reid Holmes <rtholmes@gmail.com>
2022-10-05 15:32:29 -0700

    see #421 initial backend implementation for prairielearn gradebook CSVs

--- packages/portal/backend/src/server/common/CSVParser.ts
+++ packages/portal/backend/src/server/common/CSVParser.ts
@@ -1,12 +1,13 @@
-import * as parse from 'csv-parse';
-import * as fs from 'fs';
-import Log from '../../../../../common/Log';
+import * as parse from "csv-parse";
+import * as fs from "fs";
 
-import {DatabaseController} from "../../controllers/DatabaseController";
-import {DeliverablesController} from "../../controllers/DeliverablesController";
-import {GradesController} from "../../controllers/GradesController";
-import {PersonController} from "../../controllers/PersonController";
-import {AuditLabel, Grade} from "../../Types";
+import Log from "@common/Log";
+
+import {DatabaseController} from "@backend/controllers/DatabaseController";
+import {DeliverablesController} from "@backend/controllers/DeliverablesController";
+import {GradesController} from "@backend/controllers/GradesController";
+import {PersonController} from "@backend/controllers/PersonController";
+import {AuditLabel, Grade} from "@backend/Types";
 
 export class CSVParser {
 
@@ -15,8 +16,8 @@ export class CSVParser {
     }
 
     /**
-     * Use CSV-Parse to turn a file path into an array of rows. Since we don't know anything
-     * about each row, we're just returning it as an array of any. Clients should check to
+     * Use CSV-Parse to turn a file path into an array of rows. Since we don"t know anything
+     * about each row, we"re just returning it as an array of any. Clients should check to
      * make sure the right properties exist on each row (e.g., that all the columns are there).
      *
      * @param {string} path
@@ -35,7 +36,7 @@ export class CSVParser {
 
             const parser = parse(options, (err: Error, data: any[]) => {
                 if (err) {
-                    const msg = 'CSV parse error: ' + err;
+                    const msg = "CSV parse error: " + err;
                     Log.error("CSVParser::parsePath(..) - ERROR: " + msg);
                     reject(new Error(msg));
                 } else {
@@ -51,20 +52,21 @@ export class CSVParser {
     /**
      * Process grades for a given deliverable.
      *
-     * The CSV should have three columns (each with a case-sensitive header):
+     * The default Classy format should
+     * have three columns (each with a case-sensitive header):
      *   * CSID
      *   * GRADE
      *   * COMMENT
-     *
      * If CSID is absent but CWL or GITHUB is present, we map them to the CSID and proceed as needed.
+     * The deliverable the CSV is applied to is specified by the upload page.
      *
-     * @param requestorId
+     * @param requesterId
      * @param delivId
      * @param path
      */
-    public async processGrades(requestorId: string, delivId: string, path: string): Promise<boolean[]> {
+    public async processGrades(requesterId: string, delivId: string, path: string): Promise<boolean[]> {
         try {
-            Log.info('CSVParser::processGrades(..) - start');
+            Log.info("CSVParser::processGrades(..) - start");
 
             const data = await this.parsePath(path);
 
@@ -80,28 +82,28 @@ export class CSVParser {
             const gradePromises: Array<Promise<boolean>> = [];
             for (const row of data) {
 
-                if (typeof row.CSID === 'undefined' && typeof row.GITHUB !== 'undefined') {
-                    Log.trace('CSVParser::processGrades(..) - CSID absent but GITHUB present; GITHUB: ' + row.GITHUB);
+                if (typeof row.CSID === "undefined" && typeof row.GITHUB !== "undefined") {
+                    Log.trace("CSVParser::processGrades(..) - CSID absent but GITHUB present; GITHUB: " + row.GITHUB);
                     const person = await pc.getGitHubPerson(row.GITHUB);
                     if (person !== null) {
                         row.CSID = person.csId;
-                        Log.info('CSVParser::processGrades(..) - GITHUB -> CSID: ' + row.GITHUB + " -> " + row.CSID);
+                        Log.info("CSVParser::processGrades(..) - GITHUB -> CSID: " + row.GITHUB + " -> " + row.CSID);
                     }
                 }
 
-                if (typeof row.CSID === 'undefined' && typeof row.CWL !== 'undefined') {
-                    Log.trace('CSVParser::processGrades(..) - CSID absent but CWL present; CWL: ' + row.CWL);
+                if (typeof row.CSID === "undefined" && typeof row.CWL !== "undefined") {
+                    Log.trace("CSVParser::processGrades(..) - CSID absent but CWL present; CWL: " + row.CWL);
                     const person = await pc.getGitHubPerson(row.CWL); // GITHUB && CWL are the same at UBC
                     if (person !== null) {
                         row.CSID = person.csId;
-                        Log.info('CSVParser::processGrades(..) - CWL -> CSID: ' + row.CWL + " -> " + row.CSID);
+                        Log.info("CSVParser::processGrades(..) - CWL -> CSID: " + row.CWL + " -> " + row.CSID);
                     }
                 }
 
-                // Log.trace('grade row: ' + JSON.stringify(row));
-                if (typeof row.CSID !== 'undefined' &&
-                    typeof row.GRADE !== 'undefined' &&
-                    typeof row.COMMENT !== 'undefined') {
+                // Log.trace("grade row: " + JSON.stringify(row));
+                if (typeof row.CSID !== "undefined" &&
+                    typeof row.GRADE !== "undefined" &&
+                    typeof row.COMMENT !== "undefined") {
 
                     const personId = row.CSID;
                     const g: Grade = {
@@ -110,7 +112,7 @@ export class CSVParser {
                         score:     Number(row.GRADE),
                         comment:   row.COMMENT,
                         timestamp: Date.now(),
-                        urlName:   'CSV Upload',
+                        urlName:   "CSV Upload",
                         URL:       null, // set to null so GradesController can restore URL if needed
                         custom:    {}
                     };
@@ -119,12 +121,12 @@ export class CSVParser {
                     if (person !== null) {
                         gradePromises.push(gc.saveGrade(g));
                     } else {
-                        Log.warn('CSVParser::processGrades(..) - record ignored for: ' + personId + '; unknown personId');
+                        Log.warn("CSVParser::processGrades(..) - record ignored for: " + personId + "; unknown personId");
                     }
 
                 } else {
-                    const msg = 'Required column missing (required: CSID, GRADE, COMMENT).';
-                    Log.error('CSVParser::processGrades(..) - column missing from: ' + JSON.stringify(row));
+                    const msg = "Required column missing (required: CSID, GRADE, COMMENT).";
+                    Log.error("CSVParser::processGrades(..) - column missing from: " + JSON.stringify(row));
                     throw new Error(msg);
                 }
             }
@@ -133,13 +135,13 @@ export class CSVParser {
 
             // audit grade update
             const dbc = DatabaseController.getInstance();
-            await dbc.writeAudit(AuditLabel.GRADE_ADMIN, requestorId, {}, {}, {numGrades: grades.length});
-            Log.info('CSVParser::processGrades(..) - done; # grades: ' + grades.length);
+            await dbc.writeAudit(AuditLabel.GRADE_ADMIN, requesterId, {}, {}, {numGrades: grades.length});
+            Log.info("CSVParser::processGrades(..) - done; # grades: " + grades.length);
 
             return grades;
         } catch (err) {
-            Log.error('CSVParser::processGrades(..) - ERROR: ' + err.message);
-            throw new Error('Grade upload error: ' + err.message);
+            Log.error("CSVParser::processGrades(..) - ERROR: " + err.message);
+            throw new Error("Grade upload error: " + err.message);
         }
     }
 }
---
+++ packages/portal/backend/src/server/common/CSVPrairieLearnParser.ts
@@ -0,0 +1,150 @@
+import * as parse from "csv-parse";
+import * as fs from "fs";
+
+import Log from "@common/Log";
+
+import {DatabaseController} from "@backend/controllers/DatabaseController";
+import {DeliverablesController} from "@backend/controllers/DeliverablesController";
+import {GradesController} from "@backend/controllers/GradesController";
+import {PersonController} from "@backend/controllers/PersonController";
+import {AuditLabel, Deliverable, Grade} from "@backend/Types";
+
+export class CSVPrairieLearnParser {
+
+    constructor() {
+        // empty
+    }
+
+    /**
+     * Use CSV-Parse to turn a file path into an array of rows. Since we don"t know anything
+     * about each row, we"re just returning it as an array of any. Clients should check to
+     * make sure the right properties exist on each row (e.g., that all the columns are there).
+     *
+     * @param {string} path
+     * @returns {Promise<any[]>}
+     */
+    public parsePath(path: string): Promise<any[]> {
+        return new Promise(function (fulfill, reject) {
+
+            const rs = fs.createReadStream(path);
+            const options = {
+                columns: true,
+                skip_empty_lines: true,
+                trim: true,
+                bom: true // fixes CSV compatibility issue
+            };
+
+            const parser = parse(options, (err: Error, data: any[]) => {
+                if (err) {
+                    const msg = "CSV parse error: " + err;
+                    Log.error("CSVPrairieLearnParser::parsePath(..) - ERROR: " + msg);
+                    reject(new Error(msg));
+                } else {
+                    Log.info("CSVPrairieLearnParser::parsePath(..) - parsing successful; # rows: " + data.length);
+                    fulfill(data);
+                }
+            });
+
+            rs.pipe(parser);
+        });
+    }
+
+    /**
+     * Process grades from a PrarieLearn Gradebook CSV.
+     *
+     * The PrairieLearn format has the following headers:
+     *   * UID (this is a CWL with @ubc.ca appended to the end)
+     *   * UIN (random identifier)
+     *   * Role (Student | Staff)
+     *   * Some number of columns named after assessment ids (e.g., q0, q2, etc)
+     *
+     * The deliverable the CSV is applied to depends on the assessment headers;
+     * if an assessment matches a deliverable in Classy, that grade will be set.
+     *
+     * @param requesterId
+     * @param path
+     */
+    public async processGrades(requesterId: string, path: string): Promise<boolean[]> {
+        try {
+            Log.info("CSVPrairieLearnParser::processGrades(..) - start");
+
+            const data = await this.parsePath(path);
+
+            if (data.length === 0) {
+                throw new Error("Not a valid PrairieLearn Gradebook; no rows in CSV.");
+            }
+            // validate required columns are present
+            for (const row of data) {
+                if (typeof row.UID === "undefined" &&
+                    typeof row.UIN === "undefined" &&
+                    typeof row.Role === "undefined") {
+                    throw new Error("Not a valid PrairieLearn Gradebook; required columns (UID, UIN, Role) missing.");
+                }
+            }
+
+            const dc = new DeliverablesController();
+            const allDelivs = await dc.getAllDeliverables();
+            Log.info("CSVPrairieLearnParser::processGrades(..) - # delivs: " + allDelivs.length);
+
+            const potentialDelivs: Deliverable[] = [];
+            for (const deliv of allDelivs) {
+                const row = data[0];
+                if (deliv.id !== "UID" && deliv.id !== "UIN" && deliv.id !== "Role" &&
+                    typeof row[deliv.id] !== "undefined") {
+                    // aka not a metadata row, and sheet column exists for deliv id
+                    potentialDelivs.push(deliv);
+                }
+            }
+            Log.info("CSVPrairieLearnParser::processGrades(..) - # potential delivs: " + potentialDelivs.length);
+
+            const pc = new PersonController();
+            const gc = new GradesController();
+            const gradePromises: Array<Promise<boolean>> = [];
+
+            for (const row of data) {
+
+                const uid = row.UID;
+                const cwl = uid.substring(0, uid.indexOf("@ubc.ca"));
+                Log.trace("uid: " + uid + " -> cwl: " + cwl);
+                const person = await pc.getGitHubPerson(cwl);
+
+                if (person !== null) {
+                    for (const deliv of potentialDelivs) {
+                        if (typeof row[deliv.id] !== "undefined") {
+                            const score = row[deliv.id];
+                            if (score !== "") {
+                                const g: Grade = {
+                                    personId: person.id,
+                                    delivId: deliv.id,
+                                    score: Number(score),
+                                    comment: "PrairieLearn Upload",
+                                    timestamp: Date.now(),
+                                    urlName: "CSV Upload",
+                                    URL: null, // set to null so GradesController can restore URL if needed
+                                    custom: {}
+                                };
+                                gradePromises.push(gc.saveGrade(g));
+                            } else {
+                                // blank cell
+                            }
+                        }
+                    }
+                } else {
+                    Log.warn("CSVPrairieLearnParser::processGrades(..) - unknown person: " + cwl);
+                }
+            }
+
+            const grades = await Promise.all(gradePromises);
+
+            // audit grade update
+            const dbc = DatabaseController.getInstance();
+            await dbc.writeAudit(AuditLabel.GRADE_ADMIN, requesterId, {}, {}, {numGrades: grades.length});
+            Log.info("CSVPrairieLearnParser::processGrades(..) - done; # grades: " + grades.length);
+
+            return grades;
+        } catch (err) {
+            Log.error("CSVPrairieLearnParser::processGrades(..) - ERROR: " + err.message);
+            throw new Error("Grade upload error: " + err.message);
+        }
+    }
+}
