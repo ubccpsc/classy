bc56397f2691f2f45e42ec636b382b1eca577107
reid holmes <rtholmes@cs.ubc.ca>
2025-01-08 19:37:45 -0800

    update webhook updater

--- packages/portal/backend/src-util/WebhookUpdater.ts
+++ packages/portal/backend/src-util/WebhookUpdater.ts
@@ -25,7 +25,8 @@ export class WebhookUpdater {
      * @type {string}
      */
         // private HOOK_URL = "https://localhost/portal/githubWebhook";
-    private HOOK_URL = "https://sdmm.cs.ubc.ca/portal/githubWebhook";
+        // private HOOK_URL = "https://sdmm.cs.ubc.ca/portal/githubWebhook";
+    private HOOK_URL = "https://cs310.students.cs.ubc.ca/portal/githubWebhook";
 
     private gha = GitHubActions.getInstance(true);
 
@@ -51,18 +52,24 @@ export class WebhookUpdater {
         const REPOS_TO_IGNORE = ["PostTestDoNotDelete1", "PostTestDoNotDelete", "bootstrap",
             "capstone", "project_oracle", "course-csv-dataset ", "classy"];
 
-        const reposToUpdate = [];
+        const REPOS_TO_UPDATE: string[] = ["project-sample-solution"];
+
+        const candidateRepos = [];
         const repos = await this.gha.listRepos();
         for (const repo of repos) {
             if (REPOS_TO_IGNORE.indexOf(repo.repoName) >= 0) {
-                Log.info("WebhookUpdater::updateHooks() - repo to IGNORE: " + repo.repoName);
+                Log.info("WebhookUpdater::updateHooks() - explicitly skipped; IGNORE: " + repo.repoName);
             } else {
-                Log.info("WebhookUpdater::updateHooks() - repo to UPDATE: " + repo.repoName);
-                reposToUpdate.push(repo);
+                if (REPOS_TO_UPDATE.length === 0 || REPOS_TO_UPDATE.indexOf(repo.repoName) >= 0) {
+                    Log.info("WebhookUpdater::updateHooks() - repo to UPDATE: " + repo.repoName);
+                    candidateRepos.push(repo);
+                } else {
+                    Log.info("WebhookUpdater::updateHooks() - not included; IGNORE: " + repo.repoName);
+                }
             }
         }
 
-        for (const repo of reposToUpdate) {
+        for (const repo of candidateRepos) {
             const currentHooks = await this.gha.listWebhooks(repo.repoName) as any[];
             let currHook = "_NOT-SET_";
             if (currentHooks.length === 1 && typeof currentHooks[0].config !== "undefined") {
@@ -96,9 +103,9 @@ export class WebhookUpdater {
 
 const ghc = new WebhookUpdater();
 const start = Date.now();
-ghc.run().then(function() {
+ghc.run().then(function () {
     Log.info("WebhookUpdater::run() - complete; took: " + Util.took(start));
-}).catch(function(err) {
+}).catch(function (err) {
     Log.error("GitHubCleaner::run() - ERROR: " + err.message);
     process.exit();
 });
