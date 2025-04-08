cc2a6e01fcbdc60b5b1af5c5fdb93aef64b16fe5
reid holmes <rtholmes@cs.ubc.ca>
2025-04-08 14:22:38 -0700

    see #123 add rudimentary (unstyled) support for providing student feedback.

--- packages/portal/frontend/src/app/views/AbstractStudentView.ts
+++ packages/portal/frontend/src/app/views/AbstractStudentView.ts
@@ -15,6 +15,21 @@ export abstract class AbstractStudentView implements IView {
 
 	public abstract renderPage(pageName: string, opts: {}): void;
 
+	public pushPage(pageName: string, opts: {}) {
+		Log.info("AbstractStudentView::pushPage( " + pageName + ", ... ) - start");
+		if (typeof opts !== "object") {
+			opts = {};
+		}
+		const prefix = Factory.getInstance().getHTMLPrefix();
+		UI.pushPage(prefix + "/" + pageName, opts)
+			.then(function () {
+				// success
+			})
+			.catch(function (err) {
+				Log.error("AbstractStudentView::pushPage(..) - ERROR: " + err.message);
+			});
+	}
+
 	// do general student actions
 	protected async render(): Promise<void> {
 		await this.prepareData(); // sets fields
@@ -25,6 +40,45 @@ export abstract class AbstractStudentView implements IView {
 
 	protected getStudent(): StudentTransport {
 		return this.person;
+	}
+
+	/**
+	 *
+	 * Returns null if nothing was found.
+	 * Can throw {Error}.
+	 *
+	 * @param {string} endpoint
+	 * @returns {Promise<any>}
+	 */
+	protected async fetchData(endpoint: string): Promise<any> {
+		const url = this.remote + endpoint;
+		const response = await fetch(url, this.getOptions());
+		if (response.status === 200) {
+			Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - 200 received");
+			const json = await response.json();
+			Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - payload: " + JSON.stringify(json));
+			if (typeof json.success !== "undefined") {
+				Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - success: " + json.success);
+				return json.success;
+			} else {
+				Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - ERROR: " + json.failure.message);
+				throw new Error(json.failure.message);
+			}
+		} else {
+			Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - teams !200 received");
+		}
+		return null;
+	}
+
+	protected getOptions(): { headers: { [header: string]: string } } {
+		return {
+			headers: {
+				"Content-Type": "application/json",
+				user: localStorage.user,
+				token: localStorage.token,
+				org: localStorage.org,
+			},
+		};
 	}
 
 	private async prepareData(): Promise<void> {
@@ -63,34 +117,6 @@ export abstract class AbstractStudentView implements IView {
 		}
 
 		return;
-	}
-
-	/**
-	 *
-	 * Returns null if nothing was found.
-	 * Can throw {Error}.
-	 *
-	 * @param {string} endpoint
-	 * @returns {Promise<any>}
-	 */
-	protected async fetchData(endpoint: string): Promise<any> {
-		const url = this.remote + endpoint;
-		const response = await fetch(url, this.getOptions());
-		if (response.status === 200) {
-			Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - 200 received");
-			const json = await response.json();
-			Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - payload: " + JSON.stringify(json));
-			if (typeof json.success !== "undefined") {
-				Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - success: " + json.success);
-				return json.success;
-			} else {
-				Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - ERROR: " + json.failure.message);
-				throw new Error(json.failure.message);
-			}
-		} else {
-			Log.trace("AbstractStudentView::fetchData( " + endpoint + " ) - teams !200 received");
-		}
-		return null;
 	}
 
 	private renderGrades() {
@@ -155,16 +181,40 @@ export abstract class AbstractStudentView implements IView {
 				if (comment === null) {
 					comment = "";
 				}
+
+				let commentDisplay = "";
+				let commentHTML = "";
+				if (comment !== "") {
+					// if there is a comment
+					commentDisplay = "View Comment";
+					commentHTML = this.constructFeedbackString(comment);
+				} else {
+					commentDisplay = "No Comment";
+					commentHTML = "";
+				}
+
 				const row: TableCell[] = [
 					{ value: grade.delivId, html: grade.delivId },
 					{ value: scoreToDisplay, html: scoreHTML },
-					{ value: comment, html: comment },
+					{ value: commentDisplay, html: commentHTML },
 				];
 				st.addRow(row);
 			}
 
 			st.generate();
 		}
+	}
+
+	private constructFeedbackString(comment: string): string {
+		let feedbackString = '<a href="" onclick="';
+		feedbackString += "  var data = \'<html><head><title>Feedback</title></head><body><p>";
+		feedbackString += comment;
+		feedbackString += " </p></body></html>';\n";
+		feedbackString += "  var myWindow = window.open('_blank');\n";
+		feedbackString += "  var doc = myWindow.document.open();\n";
+		feedbackString += "  doc.write(data); doc.close(); ";
+		feedbackString += '">View Feedback</a>';
+		return feedbackString;
 	}
 
 	private renderRepositories() {
@@ -214,31 +264,5 @@ export abstract class AbstractStudentView implements IView {
 
 			st.generate();
 		}
-	}
-
-	protected getOptions(): { headers: { [header: string]: string } } {
-		return {
-			headers: {
-				"Content-Type": "application/json",
-				user: localStorage.user,
-				token: localStorage.token,
-				org: localStorage.org,
-			},
-		};
-	}
-
-	public pushPage(pageName: string, opts: {}) {
-		Log.info("AbstractStudentView::pushPage( " + pageName + ", ... ) - start");
-		if (typeof opts !== "object") {
-			opts = {};
-		}
-		const prefix = Factory.getInstance().getHTMLPrefix();
-		UI.pushPage(prefix + "/" + pageName, opts)
-			.then(function () {
-				// success
-			})
-			.catch(function (err) {
-				Log.error("AbstractStudentView::pushPage(..) - ERROR: " + err.message);
-			});
 	}
 }
